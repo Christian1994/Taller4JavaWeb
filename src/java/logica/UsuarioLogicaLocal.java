004cffeeb07b6d3f25c6ffb36ff35327859aed4a
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-04-16 21:18:34 -0500

    Bosquejo de capa Lógica creada.

---
+++ src/java/logica/UsuarioLogicaLocal.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.util.List;
+import javax.ejb.Local;
+import modelo.Usuario;
+
+/**
+ *
+ * @author NOREÑA
+ */
+@Local
+public interface UsuarioLogicaLocal {
+    public void registrarUsuario(Usuario usuario) throws Exception;
+    public void modificarUsuario(Usuario usuario) throws Exception;
+    public void eliminarUsuario(Usuario usuario) throws Exception;
+    public Usuario consultarxCodigo (Integer codigo) throws Exception;
+    public List<Usuario> consultarTodas() throws Exception;    
+}
