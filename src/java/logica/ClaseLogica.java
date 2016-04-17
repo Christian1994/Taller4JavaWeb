7f7ec4f3093d7fe03d8ef4039ba943141f7519cd
Christian1994 <crisdanovagoodyear@hotmail.com>
2016-04-17 15:18:52 -0500

    Capa Lógica creada por completo. Siguen las vistas, sesiones y filtros.

--- src/java/logica/ClaseLogica.java
+++ src/java/logica/ClaseLogica.java
@@ -5,9 +5,12 @@
  */
 package logica;
 
+import java.util.Date;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import modelo.Clase;
+import persistencia.ClaseFacadeLocal;
 
 /**
  *
@@ -16,29 +19,124 @@ import modelo.Clase;
 @Stateless
 public class ClaseLogica implements ClaseLogicaLocal {
 
+    @EJB
+    ClaseFacadeLocal claseDAO;
+    
     @Override
     public void registrarClase(Clase clase) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(clase == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(clase.getNumero() == 0 || clase.getNumero() == null){
+                throw new Exception("Campo Numero obligatorio.");
+            }
+            if(clase.getFecha() == null){
+                throw new Exception("Campo Nombre obligatorio.");
+            }
+            if(clase.getAsignatura() == null || clase.getAsignatura().equals("")){
+                throw new Exception("Campo Asignatura obligatorio.");
+            }
+            if(clase.getTema() == null || clase.getTema().equals("")){
+                throw new Exception("Campo Tema obligatorio.");
+            }
+            if(clase.getHorainicio() == null){
+                throw new Exception("Campo Hora Inicio obligatorio.");
+            }
+            if(clase.getHorafin() == null){
+                throw new Exception("Campo Hora Fin obligatorio.");
+            }
+            if(clase.getProfesor() == null || clase.getProfesor().equals("")){
+                throw new Exception("Campo Profesor obligatorio.");
+            }
+        }
+        
+        Clase objClase = claseDAO.find(clase.getNumero());
+        if(objClase != null){
+            throw new Exception("Clase ya existe.");
+        }
+        else{
+            claseDAO.create(clase);
+        }
     }
 
     @Override
     public void modificarClase(Clase clase) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(clase == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(clase.getNumero() == 0 || clase.getNumero() == null){
+                throw new Exception("Campo Numero obligatorio.");
+            }
+            if(clase.getFecha() == null){
+                throw new Exception("Campo Nombre obligatorio.");
+            }
+            if(clase.getAsignatura() == null || clase.getAsignatura().equals("")){
+                throw new Exception("Campo Asignatura obligatorio.");
+            }
+            if(clase.getTema() == null || clase.getTema().equals("")){
+                throw new Exception("Campo Tema obligatorio.");
+            }
+            if(clase.getHorainicio() == null){
+                throw new Exception("Campo Hora Inicio obligatorio.");
+            }
+            if(clase.getHorafin() == null){
+                throw new Exception("Campo Hora Fin obligatorio.");
+            }
+            if(clase.getProfesor() == null || clase.getProfesor().equals("")){
+                throw new Exception("Campo Profesor obligatorio.");
+            }
+        }
+        
+        Clase objClase = claseDAO.find(clase.getNumero());
+        if(objClase == null){
+            throw new Exception("Clase a modificar no existe.");
+        }
+        else{
+            objClase.setFecha(clase.getFecha());
+            objClase.setAsignatura(clase.getAsignatura());
+            objClase.setTema(clase.getTema());
+            objClase.setHorainicio(clase.getHorainicio());
+            objClase.setHorafin(clase.getHorafin());
+            objClase.setProfesor(clase.getProfesor());
+            claseDAO.edit(clase);
+        }
     }
 
     @Override
     public void eliminarClase(Clase clase) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(clase == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(clase.getNumero() == 0 || clase.getNumero() == null){
+                throw new Exception("Campo Numero obligatorio.");
+            }
+        }
+        
+        Clase objClase = claseDAO.find(clase.getNumero());
+        if(objClase == null){
+            throw new Exception("Clase a eliminar no existe.");
+        }
+        else{
+            claseDAO.remove(clase);
+        }
     }
 
     @Override
     public Clase consultarxCodigo(Integer codigo) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(codigo == 0 || codigo == null){
+            throw new Exception("Campo Documento obligatorio.");
+        }
+        else{
+            return claseDAO.find(codigo);
+        }
     }
 
     @Override
     public List<Clase> consultarTodas() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return claseDAO.findAll();
     }
 
     // Add business logic below. (Right-click in editor and choose
--- src/java/logica/DocenteLogica.java
+++ src/java/logica/DocenteLogica.java
@@ -6,8 +6,10 @@
 package logica;
 
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import modelo.Docente;
+import persistencia.DocenteFacadeLocal;
 
 /**
  *
@@ -16,29 +18,110 @@ import modelo.Docente;
 @Stateless
 public class DocenteLogica implements DocenteLogicaLocal {
 
+    @EJB
+    DocenteFacadeLocal docenteDAO;
+    
     @Override
     public void registrarDocente(Docente docente) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(docente == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(docente.getDocumento() == 0 || docente.getDocumento() == null){
+                throw new Exception("Campo Documento obligatorio.");
+            }
+            if(docente.getNombre() == null || docente.getNombre().equals("")){
+                throw new Exception("Campo Nombre obligatorio.");
+            }
+            if(docente.getApellido() == null || docente.getApellido().equals("")){
+                throw new Exception("Campo Docente obligatorio.");
+            }
+            if(docente.getCorreo() == null || docente.getCorreo().equals("")){
+                throw new Exception("Campo Correo obligatorio.");
+            }
+            if(docente.getTelefono() == 0){
+                throw new Exception("Campo Teléfono obligatorio.");
+            }
+        }
+        
+        Docente objDocente = docenteDAO.find(docente.getDocumento());
+        if(objDocente != null){
+            throw new Exception("Docente ya existe.");
+        }
+        else{
+            docenteDAO.create(docente);
+        }
     }
 
     @Override
     public void modificarDocente(Docente docente) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(docente == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(docente.getDocumento() == 0 || docente.getDocumento() == null){
+                throw new Exception("Campo Documento obligatorio.");
+            }
+            if(docente.getNombre() == null || docente.getNombre().equals("")){
+                throw new Exception("Campo Nombre obligatorio.");
+            }
+            if(docente.getApellido() == null || docente.getApellido().equals("")){
+                throw new Exception("Campo Docente obligatorio.");
+            }
+            if(docente.getCorreo() == null || docente.getCorreo().equals("")){
+                throw new Exception("Campo Correo obligatorio.");
+            }
+            if(docente.getTelefono() == 0){
+                throw new Exception("Campo Teléfono obligatorio.");
+            }
+        }
+        
+        Docente objDocente = docenteDAO.find(docente.getDocumento());
+        if(objDocente == null){
+            throw new Exception("Docente a modificar no existe.");
+        }
+        else{
+            objDocente.setNombre(docente.getNombre());
+            objDocente.setApellido(docente.getApellido());
+            objDocente.setCorreo(docente.getCorreo());
+            objDocente.setTelefono(docente.getTelefono());
+            docenteDAO.edit(docente);
+        }
     }
 
     @Override
     public void eliminarDocente(Docente docente) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(docente == null){
+            throw new Exception("Campos vacíos.");
+        }
+        else{
+            if(docente.getDocumento() == 0 || docente.getDocumento() == null){
+                throw new Exception("Campo Documento obligatorio.");
+            }
+        }
+        
+        Docente objDocente = docenteDAO.find(docente.getDocumento());
+        if(objDocente == null){
+            throw new Exception("Docente a eliminar no existe.");
+        }
+        else{
+            docenteDAO.remove(docente);
+        }
     }
 
     @Override
     public Docente consultarxDocumento(Integer documento) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(documento == 0 || documento == null){
+            throw new Exception("Campo Documento obligatorio.");
+        }
+        else{
+            return docenteDAO.find(documento);
+        }
     }
 
     @Override
     public List<Docente> consultarTodos() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return docenteDAO.findAll();
     }
 
     // Add business logic below. (Right-click in editor and choose
